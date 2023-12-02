efb3c62b054a9f0c1f829d16e72c9107e02d5059
shehanm04 <shehan-m@outlook.com>
2023-12-02 01:28:56 +0000

    Update firmware.py

--- Firmware/firmware.py
+++ Firmware/firmware.py
@@ -1,19 +1,56 @@
 from target_detector import TargetDetector
 from stepper_motor import StepperMotorController
 import RPi.GPIO as GPIO
+import threading
+import time
 
 def limit_switch_event():
-    pass
+    # TODO Add limit switch event listener
+    return None
 
-td = TargetDetector()
+def run_alignment(detector):
+    detector.align_target()
+
+detector = TargetDetector()
 controller = StepperMotorController()
 
+max_consecutive_zeros = 5
+consecutive_zeros = 0
+
+not_detected = True
+
 # Move to wall
-while True:
+while limit_switch_event():
     controller.move(1000)
 
 # Move back from wall
+controller.move()
+
+# Align datum with target
 
 # Use target alignment
+# Main thread continuously checks y_displacement
+detection_thread = threading.Thread(target=detector.align_target)
+detection_thread.start()
+
+# Once Target is aligned stop
+# TODO make the motor move depending on displacemnt
+while not_detected:
+    y_displacement = detector.get_y_displacement()
+    print("Y Displacement:", y_displacement)
+
+    # Add any other processing or conditions as needed
+    time.sleep(0.5)
+
+    # Stop detection if needed (e.g., after x consecutive zeros)
+    if y_displacement == 0:
+        consecutive_zeros += 1
+        if consecutive_zeros >= max_consecutive_zeros:
+            detector.stop_detection()
+            not_detected = False
+            detection_thread.join()  # Wait for the thread to finish gracefully
+            break
+    else:
+        consecutive_zeros = 0
 
 # Move to next target
