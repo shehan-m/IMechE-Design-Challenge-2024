a67c9e4257d8c1e9ce22424be6e18b0666afa752
Shehan Medagedara <shehanm04@outlook.com>
2023-12-22 19:40:12 +0000

    Update stepper_motor.py
    Changed the stepper motor controller to work with new firmware

--- Firmware/stepper_motor.py
+++ Firmware/stepper_motor.py
@@ -7,6 +7,8 @@ class StepperMotorController:
         self.dir_pin = dir_pin
         self.ms_pins = [ms1_pin, ms2_pin, ms3_pin]
 
+        self.current_speed = 0.01  # Default speed, you can change this as needed
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.step_pin, GPIO.OUT)
         GPIO.setup(self.dir_pin, GPIO.OUT)
@@ -29,19 +31,41 @@ class StepperMotorController:
         for pin, value in zip(self.ms_pins, settings):
             GPIO.output(pin, value)
 
-    def step(self, steps, dir, speed=1):
+    def set_speed(self, speed):
+        """
+        Set the speed of the motor.
+        speed - delay between steps in seconds, smaller is faster.
+        """
+        self.current_speed = speed
+
+    def get_speed(self):
+        """
+        Get the current speed of the motor.
+        Returns the delay between steps in seconds.
+        """
+        return self.current_speed
+
+    def step(self, steps, dir):
         """
         Move the motor.
         steps - number of steps to move.
         dir - direction, either 1 (forward) or -1 (backward).
-        speed - delay between steps in seconds, smaller is faster.
         """
         GPIO.output(self.dir_pin, GPIO.HIGH if dir > 0 else GPIO.LOW)
         for _ in range(steps):
             GPIO.output(self.step_pin, GPIO.HIGH)
-            time.sleep(speed)
+            time.sleep(self.get_speed())
             GPIO.output(self.step_pin, GPIO.LOW)
-            time.sleep(speed)
+            time.sleep(self.get_speed())
+
+    def stop(self):
+        """
+        Immediately stop the motor by disabling the step pin.
+        """
+        GPIO.output(self.step_pin, GPIO.LOW)
 
     def cleanup(self):
+        """
+        Clean up by resetting the GPIO pins.
+        """
         GPIO.cleanup()
