155e80ff30ccc0ff3f062672c2a1c5b62f85888c
Shehan Medagedara <shehanm04@outlook.com>
2024-05-18 13:23:45 +0100

    Update motor.py

--- Firmware/Tests/motor.py
+++ Firmware/Tests/motor.py
@@ -9,16 +9,8 @@ pi.set_mode(STEP_PIN, pigpio.OUTPUT)
 pi.set_mode(DIR_PIN, pigpio.OUTPUT)
 
 def move_motor(start_frequency, final_frequency, steps, dir=1, run_time=None):
-    """Generate ramp waveforms from start to final frequency.
-    
-    Parameters:
-    - start_frequency: Starting frequency of the ramp.
-    - final_frequency: Ending frequency of the ramp.
-    - steps: Number of steps in the ramp.
-    - dir: Direction to move the motor (0 or 1).
-    - run_time: Time in seconds to run the motor at final frequency, None for indefinite run.
-    """
-    
+    global last_wave_ids  # Declare global at the start of the function
+
     if not pi.connected:
         print("Error connecting to pigpio daemon. Is the daemon running?")
         return
@@ -59,24 +51,9 @@ def move_motor(start_frequency, final_frequency, steps, dir=1, run_time=None):
     else:
         pi.wave_send_repeat(wid[-1])
 
-    global last_wave_ids
     last_wave_ids = wid
 
-    # Handle run time
-    if run_time is not None:
-        pi.wave_send_repeat(wid[-1])
-        time.sleep(run_time)
-        pi.wave_tx_stop()  # Stop waveform transmission
-    else:
-        # If no run_time specified, repeat the last waveform indefinitely
-        pi.wave_send_repeat(wid[-1])
-
-    # Clean up waveforms
-    global last_wave_ids
-    last_wave_ids = wid  # Store wave IDs globally to allow stopping later
-
 def stop_motor():
-    """Stop any running waveforms and clean up."""
     global last_wave_ids
     pi.wave_tx_stop()  # Stop any waveform transmission
     if last_wave_ids is not None:
